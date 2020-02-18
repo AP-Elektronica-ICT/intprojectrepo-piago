8ebbaf0045b00f6e2e3a887eee32544ae0d15476
Stijn Roscam <stijn.roscam@student.ap.be>
2020-02-18 10:34:24 +0100

    Login screen layout

---
+++ PiaGOApp/app/src/main/java/com/example/piago/PiagoMainScreen.java
@@ -0,0 +1,14 @@
+package com.example.piago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PiagoMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piago_main_screen);
+    }
+}

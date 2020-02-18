e78aa767e68824cf36bc33ae4a7781828e6c541a
Arnaud Claes <arnaud.claes@student.ap.be>
2020-02-18 10:10:02 +0100

    Added BT activity

---
+++ PiaGOApp/app/src/main/java/com/example/piago/BluetoothActivity.java
@@ -0,0 +1,17 @@
+package com.example.piago;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
+
+public class BluetoothActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+
+
+    }
+}

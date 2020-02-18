a882c204d7f17a70c262f2265227ccec8dd07700
Stijn Roscam <stijn.roscam@student.ap.be>
2020-02-18 10:45:56 +0100

    mainscreen layout

--- PiaGOApp/app/src/main/java/com/example/piago/PiagoMainScreen.java
+++ PiaGOApp/app/src/main/java/com/example/piago/PiagoMainScreen.java
@@ -3,6 +3,7 @@ package com.example.piago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class PiagoMainScreen extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class PiagoMainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piago_main_screen);
+    }
+
+    public void launchJam(View view) {
+    }
+
+    public void launchLearn(View view) {
+    }
+
+    public void launchCustomize(View view) {
     }
 }

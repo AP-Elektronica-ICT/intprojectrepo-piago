76426ddc0bf908450579f2ae9815dcefc3e58b73
Stijn Roscam <stijn.roscam@student.ap.be>
2020-03-21 11:58:20 +0100

    Created octave Selector

--- PiaGOApp/app/src/main/java/com/interproject/piago/PlayPiago.java
+++ PiaGOApp/app/src/main/java/com/interproject/piago/PlayPiago.java
@@ -29,6 +29,8 @@ public class PlayPiago extends AppCompatActivity {
 
     public String ReceivedBluetoothSignal;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

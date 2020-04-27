8163c77d9ca30e9ebc7d3813c679f5475335fd73
Benoît <benoitlagasse@hotmail.com>
2020-04-27 23:34:04 +0200

    changed settings name

--- PiaGo_CSharp/PiaGo_Master/PiaGo_CSharp/SettingsScreen.Designer.cs
+++ PiaGo_CSharp/PiaGo_Master/PiaGo_CSharp/SettingsScreen.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsScreen));
             this.metroSMSettings = new MetroFramework.Components.MetroStyleManager(this.components);
             this.tglMetroTheme = new MetroFramework.Controls.MetroToggle();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
@@ -47,92 +48,62 @@
             // 
             // tglMetroTheme
             // 
-            this.tglMetroTheme.AutoSize = true;
-            this.tglMetroTheme.Location = new System.Drawing.Point(227, 130);
+            resources.ApplyResources(this.tglMetroTheme, "tglMetroTheme");
             this.tglMetroTheme.Name = "tglMetroTheme";
-            this.tglMetroTheme.Size = new System.Drawing.Size(80, 17);
-            this.tglMetroTheme.TabIndex = 7;
-            this.tglMetroTheme.Text = "Off";
             this.tglMetroTheme.UseSelectable = true;
             this.tglMetroTheme.CheckedChanged += new System.EventHandler(this.metroToggle1_CheckedChanged);
             // 
             // metroLabel1
             // 
-            this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(15, 128);
+            resources.ApplyResources(this.metroLabel1, "metroLabel1");
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(80, 19);
-            this.metroLabel1.TabIndex = 8;
-            this.metroLabel1.Text = "Dark theme:";
             // 
             // btnMetroCPBG
             // 
-            this.btnMetroCPBG.Location = new System.Drawing.Point(232, 68);
+            resources.ApplyResources(this.btnMetroCPBG, "btnMetroCPBG");
             this.btnMetroCPBG.Name = "btnMetroCPBG";
-            this.btnMetroCPBG.Size = new System.Drawing.Size(75, 23);
-            this.btnMetroCPBG.TabIndex = 9;
-            this.btnMetroCPBG.Text = "Open File";
             this.btnMetroCPBG.UseSelectable = true;
             this.btnMetroCPBG.Click += new System.EventHandler(this.btnMetroCPBG_Click);
             // 
             // metroLabel2
             // 
-            this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(15, 68);
+            resources.ApplyResources(this.metroLabel2, "metroLabel2");
             this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(212, 19);
-            this.metroLabel2.TabIndex = 10;
-            this.metroLabel2.Text = "Change control panel background:";
             // 
             // metroLabel3
             // 
-            this.metroLabel3.AutoSize = true;
-            this.metroLabel3.Location = new System.Drawing.Point(15, 94);
+            resources.ApplyResources(this.metroLabel3, "metroLabel3");
             this.metroLabel3.Name = "metroLabel3";
-            this.metroLabel3.Size = new System.Drawing.Size(168, 19);
-            this.metroLabel3.TabIndex = 11;
-            this.metroLabel3.Text = "Change piano background:";
             // 
             // btnMetroPiaonoBG
             // 
-            this.btnMetroPiaonoBG.Location = new System.Drawing.Point(232, 94);
+            resources.ApplyResources(this.btnMetroPiaonoBG, "btnMetroPiaonoBG");
             this.btnMetroPiaonoBG.Name = "btnMetroPiaonoBG";
-            this.btnMetroPiaonoBG.Size = new System.Drawing.Size(75, 23);
-            this.btnMetroPiaonoBG.TabIndex = 12;
-            this.btnMetroPiaonoBG.Text = "Open File";
             this.btnMetroPiaonoBG.UseSelectable = true;
             this.btnMetroPiaonoBG.Click += new System.EventHandler(this.btnMetroPiaonoBG_Click);
             // 
             // metroLabel4
             // 
-            this.metroLabel4.AutoSize = true;
-            this.metroLabel4.Location = new System.Drawing.Point(15, 164);
+            resources.ApplyResources(this.metroLabel4, "metroLabel4");
             this.metroLabel4.Name = "metroLabel4";
-            this.metroLabel4.Size = new System.Drawing.Size(90, 19);
-            this.metroLabel4.TabIndex = 15;
-            this.metroLabel4.Text = "Theme Color:";
             // 
             // cbmetroColor
             // 
             this.cbmetroColor.FormattingEnabled = true;
-            this.cbmetroColor.ItemHeight = 23;
+            resources.ApplyResources(this.cbmetroColor, "cbmetroColor");
             this.cbmetroColor.Items.AddRange(new object[] {
-            "RED",
-            "GREEN",
-            "BLUE",
-            "YELLOW"});
-            this.cbmetroColor.Location = new System.Drawing.Point(186, 164);
+            resources.GetString("cbmetroColor.Items"),
+            resources.GetString("cbmetroColor.Items1"),
+            resources.GetString("cbmetroColor.Items2"),
+            resources.GetString("cbmetroColor.Items3")});
             this.cbmetroColor.Name = "cbmetroColor";
-            this.cbmetroColor.Size = new System.Drawing.Size(121, 29);
-            this.cbmetroColor.TabIndex = 16;
             this.cbmetroColor.UseSelectable = true;
             this.cbmetroColor.SelectedIndexChanged += new System.EventHandler(this.cbmetroColor_SelectedIndexChanged_1);
             // 
             // SettingsScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(337, 230);
             this.Controls.Add(this.cbmetroColor);
             this.Controls.Add(this.metroLabel4);
             this.Controls.Add(this.btnMetroPiaonoBG);
@@ -143,7 +114,6 @@
             this.Controls.Add(this.tglMetroTheme);
             this.Name = "SettingsScreen";
             this.Resizable = false;
-            this.Text = "SettingsScreen";
             this.Load += new System.EventHandler(this.SettingsScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.metroSMSettings)).EndInit();
             this.ResumeLayout(false);
